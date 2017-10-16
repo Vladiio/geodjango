19a98e9ed3c9a5a9026791929e15c4f1aa3676ab
Vlad Mashkin <vlad.mashkin@anvileight.com>
2017-10-16 14:45:04 +0300

    Return items located in radius 5km near a user ordered by distance

--- world/schema.py
+++ world/schema.py
@@ -1,4 +1,6 @@
 from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
+from django.contrib.gis.db.models.functions import Distance
 
 import graphene
 from graphql.language import ast
@@ -18,12 +20,11 @@ class LocationPoint(graphene.Scalar):
     def parse_literal(node):
         if isinstance(node, ast.ListValue):
             values = [float(location.value) for location in node.values]
-            latitude, longitude = values
-            return Point(latitude, longitude)
+            return Point(*values)
 
     @staticmethod
     def parse_value(value):
-        return value
+        return Point(*value)
 
 
 class GraphItem(DjangoObjectType):
@@ -35,12 +36,17 @@ class GraphItem(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
+    # TODO create user field
     closest_items = graphene.List(GraphItem, user_location=graphene.Argument(LocationPoint))
 
     def resolve_closest_items(self, context, request, info):
+        distance = 5000
         user_location = context.get('user_location')
         if user_location is not None:
-            return Item.objects.all()
+            return Item.objects.filter(
+                location__distance_lte=(user_location, D(m=1000000))
+                ).annotate(distance=Distance('location', user_location)
+                ).order_by('distance')
 
 
 schema = graphene.Schema(query=Query)
