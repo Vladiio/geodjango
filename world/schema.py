5f0de16fa0a7898b0132630f937c1b7e6ac54781
Vlad Mashkin <vlad.mashkin@anvileight.com>
2017-10-16 16:25:12 +0300

    Refactor item location field and add user type

--- world/schema.py
+++ world/schema.py
@@ -1,6 +1,9 @@
+import json
+
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 from django.contrib.gis.db.models.functions import Distance
+from django.contrib.auth import get_user_model
 
 import graphene
 from graphql.language import ast
@@ -9,40 +12,39 @@ from graphene_django.types import DjangoObjectType
 from world.models import Item
 
 
-class LocationPoint(graphene.Scalar):
-    # custom field for location data
-
-    @staticmethod
-    def serialize(point):
-        return [point.x, point.y]
-
-    @staticmethod
-    def parse_literal(node):
-        if isinstance(node, ast.ListValue):
-            values = [float(location.value) for location in node.values]
-            return Point(*values)
-
-    @staticmethod
-    def parse_value(value):
-        return Point(*value)
+User = get_user_model()
 
 
 class GraphItem(DjangoObjectType):
 
-    location = LocationPoint()
+    location = graphene.List(graphene.Float)
 
     class Meta:
         model = Item
 
 
+class GraphUser(DjangoObjectType):
+
+    location = graphene.List(graphene.Float)
+
+    class Meta:
+        model = User
+
+
 class Query(graphene.ObjectType):
-    # TODO create user field
-    closest_items = graphene.List(GraphItem, user_location=graphene.Argument(LocationPoint))
+
+    closest_items = graphene.List(GraphItem,
+                                  user_location=graphene.Argument(graphene.List(graphene.Float)))
+    user = graphene.List(GraphUser)
+
+    def resolve_user(self, context, request, info):
+        return User.objects.all()
 
     def resolve_closest_items(self, context, request, info):
         distance = 5000
-        user_location = context.get('user_location')
-        if user_location is not None:
+        user_coords = context.get('user_location')
+        if user_coords is not None:
+            user_location = Point(*user_coords)
             return Item.objects.filter(
                 location__distance_lte=(user_location, D(m=1000000))
                 ).annotate(distance=Distance('location', user_location)
