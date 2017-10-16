6877ab3d53cc000f09c92c433c49ee644bd3fd13
Vlad Mashkin <vlad.mashkin@anvileight.com>
2017-10-16 18:20:19 +0300

    Provide home page

--- task/urls.py
+++ task/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
+    url(r'$^', TemplateView.as_view(template_name="home.html"), name="home"),
     url(r'^admin/', admin.site.urls),
     url(r'^graphql', GraphQLView.as_view(graphiql=True))
 ]
