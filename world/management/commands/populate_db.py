14318a533d039cee64b44b5a1e6fc0c04abca8d8
Vlad Mashkin <vlad.mashkin@anvileight.com>
2017-10-20 17:14:39 +0300

    Fix populate script

--- world/management/commands/populate_db.py
+++ world/management/commands/populate_db.py
@@ -10,12 +10,15 @@ from world.models import Item
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        for item_name in ('Bakery', 'Butcher\'s', 'Chemist\'s'):
-            difference = random.choice((0.02, 0.05, 0.0001, 0.000004))
+        default_lat = settings.DEFAULT_LOCATION['lat']
+        default_lng = settings.DEFAULT_LOCATION['lng']
 
-            lat = settings.DEFAULT_LOCATION['lat'] - difference
-            lng = settings.DEFAULT_LOCATION['lng'] - difference
-            point = Point(lat, lng)
+        for item_name in ('Bakery', 'Butcher\'s', 'Chemist\'s'):
+            difference = random.choice((0.02, 0.05, 0.0002, 0.0004))
+
+            item_lat = default_lat - difference
+            item_lng = default_lng + difference
+            point = Point(item_lat, item_lng)
 
             Item.objects.create(name=item_name, location=point)
         self.stdout.write(self.style.SUCCESS('Successefuly created 3 items'))
