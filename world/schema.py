31e5823a48a53ef41cde05abf5f0bf82e96af618
Vlad Mashkin <vlad.mashkin@anvileight.com>
2017-10-20 17:16:40 +0300

    Create mutations for create users, login and logout

--- world/schema.py
+++ world/schema.py
@@ -3,6 +3,7 @@ from django.contrib.gis.measure import D
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
+from django.conf import settings
 
 
 import graphene
@@ -12,6 +13,17 @@ from world.models import Item
 
 
 User = get_user_model()
+
+
+def get_user(context):
+    token = context.session.get('token')
+    if not token:
+        return
+    try:
+        user = User.objects.get(token=token)
+        return user
+    except:
+        raise Exception('User not found')
 
 
 class GraphItem(DjangoObjectType):
@@ -43,27 +55,82 @@ class GraphUser(DjangoObjectType):
                 ).order_by('distance')
 
 
-class Query(graphene.ObjectType):
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(GraphUser)
 
-    user = graphene.Field(
-        GraphUser,
-        username=graphene.Argument(graphene.String, required=True),
-        password=graphene.Argument(graphene.String, required=True)
-    )
+    class Input:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
 
-    def resolve_user(self, context, request, info):
-        username = context.get("username", "")
-        password = context.get("password", "")
+    @staticmethod
+    def mutate(root, input, context, info):
+        username = input.get('username')
+        password = input.get('password')
 
-        user = None
-        if not request.user.is_authenticated:
-            user = authenticate(request,
-                                username=username,
-                                password=password)
-        else:
-            user = request.user
+        user = User(username=username)
+        user.location = Point(x=settings.DEFAULT_LOCATION.get('lat'),
+                              y=settings.DEFAULT_LOCATION.get('lng'))
+        user.set_password(password)
+        user.save()
+        return CreateUser(user=user)
+
+
+class LogIn(graphene.Mutation):
+    user = graphene.Field(GraphUser)
+
+    class Input:
+        username = graphene.String()
+        password = graphene.String()
+
+    @staticmethod
+    def mutate(root, input, context, info):
+        user = authenticate(
+            username=input.get('username'),
+            password=input.get('password')
+        )
+
+        if not user:
+            raise Exception('Invalid username or password')
+        context.session['token'] = user.token
+        return LogIn(user=user)
+
+
+class LogOut(graphene.Mutation):
+    status = graphene.String()
+
+    @staticmethod
+    def mutate(root, input, context, info):
+        token = context.session.get('token')
+        if not token:
+            raise Exception('You are not logged in')
+        del context.session['token']
+        return LogOut(status="Logged Out")
+
+
+class UserMutation(graphene.AbstractType):
+    create_user = CreateUser.Field()
+    login = LogIn.Field()
+    logout = LogOut.Field()
+
+
+class UserQuery(graphene.AbstractType):
+
+    user = graphene.Field(GraphUser)
+
+    def resolve_user(self, args, context, info):
+        user = get_user(context)
+        if not user:
+            raise Exception('Not logged')
 
         return user
 
 
-schema = graphene.Schema(query=Query)
+class Query(UserQuery, graphene.ObjectType):
+    pass
+
+
+class Mutation(UserMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
