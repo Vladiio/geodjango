be501092d614fe1bd26de462bf17c87863b2df0d
Vlad Mashkin <vlad.mashkin@anvileight.com>
2017-10-20 17:15:43 +0300

    Add token field to user

--- world/models.py
+++ world/models.py
@@ -1,10 +1,19 @@
+import binascii
+import os
+
 from django.db import models
 from django.contrib.gis.db import models as geo_models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    location = geo_models.PointField(blank=True, null=True)
+    location = geo_models.PointField()
+    token = models.CharField(max_length=40)
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = binascii.hexlify(os.urandom(20)).decode()
+        super().save(*args, **kwargs)
 
 
 class Item(models.Model):
